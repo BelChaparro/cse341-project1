8855031af9b963d6d12796b0db559a8c805a13b4
BelChaparro <a.belenchaparro@gmail.com>
2023-09-23 21:15:47 -0300

    added routes for post, put and delete dunctions in controller

--- routes/contacts.js
+++ routes/contacts.js
@@ -2,7 +2,17 @@ const express = require('express');
 const router = express.Router();
 const usersController = require('../controllers/contacts');
 
+// GET requests for all contacts and a single contact:
 router.get('/', usersController.getAll);
 router.get('/:id', usersController.getSingle);
+
+// POST request:
+router.post('/', usersController.createContact);
+
+// PUT request:
+router.put('/:id', usersController.updateContact);
+
+// DELETE request:
+router.delete('/:id', usersController.deleteContact);
 
 module.exports = router;
